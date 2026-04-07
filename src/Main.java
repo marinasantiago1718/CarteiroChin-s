a6bb14f248faa6b6e7d7c2e3f401d08240275d18
Marina Santiago <marinasantiago1718@gmail.com>
2026-04-07 18:20:09 -0300

    organização da main

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // criando o digrafo original
-
         In in = new In("dados/entradaOriginal.txt");
         EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
 
-
-
-        // Verificação de Graus do digrafo original
 
         StdOut.println("=== Verificação de Balanceamento Digrafo original===\n");
         boolean isBalanced = true;
         for (int v = 0; v < G.V(); v++) {
             int inDegree = 0;
-            // Cálculo manual do in-degree (grau de entrada)
+            // grau de entrada
             for (DirectedEdge e : G.edges()) {
                 if (e.to() == v) inDegree++;
             }
@@ -31,20 +26,16 @@ public class Main {
             StdOut.println("O grafo original NÃO está balanceado. \n");
         }
 
-        //identificar os vértices desbalanceados;
+        // vértices desbalanceados;
 
         System.out.println("vertices 0 , 1 , 4 , 5 estão desbalanceados\n");
 
-        //justificar quais arestas ou caminhos precisam ser acrescentados;
-
         System.out.println("Para eulerizar o grafo, foram adicionadas 6 arestas tracejadas que representam os caminhos mínimos necessários para conectar os nós desbalanceados, garantindo que o grau de entrada seja igual ao grau de saída em todos os vértices.\n");
 
-        // cirando o digrafo ponderado com a entrada balanciada
+        // entrada balanciada
 
         In in2 = new In("dados/entradaEulerizada.txt");
         EdgeWeightedDigraph GEuler = new EdgeWeightedDigraph(in2);
-
-        // informar os graus de entrada e saída dos vértices do digrafo balanciado e verificar se o grafo está balanceado;
 
         StdOut.println("=== Verificação de Balanceamento ===\n");
         boolean isBalanced2 = true;
@@ -65,7 +56,6 @@ public class Main {
             return;
         }
 
-        // Converter para Digraph
 
         Digraph D = new Digraph(GEuler.V());
 
@@ -73,7 +63,7 @@ public class Main {
             D.addEdge(e.from(), e.to());
         }
 
-        // Executar Hierholzer
+        // Hierholzer
 
         DirectedEulerianCycle euler = new DirectedEulerianCycle(D);
 
@@ -82,7 +72,6 @@ public class Main {
             return;
         }
 
-        //Imprimir o circuito
 
         StdOut.println("=== Circuito Euleriano ===\n");
 
@@ -94,7 +83,7 @@ public class Main {
         }
         StdOut.println();
 
-        //Calcular o custo total
+        //custo total
 
         double totalCost = 0.0;
 
@@ -103,18 +92,15 @@ public class Main {
         for (int v : cycle) {
             if (prevVertex != null) {
 
-                // procurar aresta prevVertex -> v
                 for (DirectedEdge e : GEuler.adj(prevVertex)) {
                     if (e.to() == v) {
                         totalCost += e.weight();
-                        break; // importante: pega só uma ocorrência
+                        break;
                     }
                 }
             }
             prevVertex = v;
         }
-
-        //mostrar custo total
 
         StdOut.println("Custo total do circuito: " + totalCost);
 
